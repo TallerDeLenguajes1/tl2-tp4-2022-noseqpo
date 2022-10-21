c890e239cf5cb6f5d2ab6bbf3e9179fbb3b1e273
Daniel Paz <danielnicolaspeace@gmail.com>
2022-10-21 17:28:17 -0300

    aprendiendo mvc

---
+++ TP04/TP04/Controllers/Test.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TP04.Controllers
+{
+    public class test : Controller
+    {
+        
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+
+    }
+}
+
